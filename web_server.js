151d5d7cc671f6d55d82e4d92b9d8e24db31b709
Vishwa prasanth <vishwaprasanth1411@gmail.com>
2024-01-25 23:58:07 +0530

    routing

--- web_server.js
+++ web_server.js
@@ -25,44 +25,12 @@ app.use(cors(corsOptions))
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/',express.static(path.join(__dirname, './public')));
+app.use('/subdir',express.static(path.join(__dirname, './public')));
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use('/', require('./routes/root'))
+app.use('/subdir', require('./routes/subdir.js'))
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, 'new-page.html');
-});
-
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('hello.html page trying to load');
-    next();
-}, (req, res) => {
-    res.send('hello... ');
-});
-
-const one = (req, res, next) => {
-    console.log('one')
-    next()
-}
-
-const two = (req, res, next) => {
-    console.log('two')
-    next()
-}
-
-const three = (req, res) => {
-    console.log('three')
-    res.send('Finished!')
-}
-
-app.get('/chain(.html)?',[one, two, three])
 
 app.get('/*', (req, res) => {
    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
